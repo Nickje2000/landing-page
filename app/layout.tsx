43c972218a213e8a9ee8fd04bf47a972cae4eb02
Nickje2000 <164410897+Nickje2000@users.noreply.github.com>
2025-02-19 22:59:29 +0100

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'My socials :D',
+  description: 'This is my (nickje2000) landing page :P',
 }
 
 export default function RootLayout({
